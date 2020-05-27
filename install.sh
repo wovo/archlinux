1ada27f8de315e81de1bdfc1b10e170efca5d7f2
Wouter van Ooijen <wouter@voti.nl>
2020-05-27 17:13:51 +0200

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 loadkeys us
-mkdir curl
-cd curl
 curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash
-cp ../alis.conf.custom alis.conf
+cp archlinux/alis.conf.custom alis.conf
 ./alis.sh
